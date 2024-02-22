93ff985fe5d2b9abd867b08670cc86d5824bc194
aashish.bhusal <aashish.bhusal@estudiantat.upc.edu>
2024-02-22 23:54:31 +0100

    A new class Order added that implements the logic of ordering a drink

---
+++ coffe-machine/coffe-machine/src/Console/Order.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This class implements the logic of ordering a drink from a machine
+ * A drink can or cannot have sugar in it 
+ * A drink can be extrahot or not 
+ * If a drink has a valid number of sugar then it has a stick
+ */
+
+ namespace Pdpaola\CoffeeMachine\Console;
+
+ class Order{
+
+    private Drink $drink; //a drink associated with a specific order
+
+    private int $sugars;
+
+    private bool $extraHot;
+
+    private bool $stick; //If a drink has a valid number of sugar then it has a stick
+
+    /**
+     * Constructor of the class.
+     */
+
+    public function __construct(Drink $drink, int $sugars, bool $extraHot){
+        $this->drink = $drink;  
+        $this->sugars = $sugars;
+        $this->extraHot = $extraHot;
+        $this->stick = $sugars > 0;
+
+    }
+
+    /**
+     * Getters of the class Order
+     */
+
+    public function getDrink(): Drink{
+        return $this->drink;
+    }
+
+    public function getSugars(): int{
+        return $this->sugars;
+    }
+
+    public function getExtraHot(): bool{
+        return $this->extraHot;
+    }
+
+    public function getStick(): int{
+        return $this->stick;
+    }
+
+
+ }
